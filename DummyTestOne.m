a53624389b06a7781a146ba014a05fd5269dc3c9
BuckeyeVerb <kbmcnich@gmail.com>
2013-10-04 13:09:07 -0400

    Updated DummyTestOne.m
    Changes made 10/1 at group meeting.  Added reps argument, changed
    impulse to be a sinusoid with an exponential envelope.  This feature
    will likely soon be chanced again to being a random signal with an
    exponential envelope.

--- DummyTestOne.m
+++ DummyTestOne.m
@@ -1,7 +1,7 @@
-function [x,h,h1,h2,y] = DummyTestOne(N,s)
+function [x,h,h1,h2,y] = DummyTestOne(reps,N,s)
 %Dummy Test One -Two different ways of extracting impulse responses from simulated data.
 %
-%syntax: [x,h,h1,h2,y] = DummyTestOne(N,s)
+%syntax: [x,h,h1,h2,y] = DummyTestOne(reps,N,s)
 %
 % Output Variables:
 % x is the generated MLS sequence using GenerateMLS.m (no repititions)
@@ -11,6 +11,7 @@ function [x,h,h1,h2,y] = DummyTestOne(N,s)
 % y is the convolution of the two signals
 %
 % Input Variables:
+% reps is the number of MLS repetitions (minimum 2)
 % N is the order of the MLS sequence
 % s is the level of noise in the output. 0 corresponds to no SNR = inf, 1 is
 % SNR = 1
@@ -32,38 +33,37 @@ function [x,h,h1,h2,y] = DummyTestOne(N,s)
 %Generate MLS sequence
 P=2^N - 1; %length of MLS sequence
 
-x = GenerateMLS(N,1);
-x=x';
+[x burst] = GenerateMLSSequence(reps,N,0);
 
 %Generate known impulse response h(t)
-
-h = rand(1,P)- rand(1,P);
-h=h';
+n=[1:P];
+h = exp(-.1*n).*cos(n);
 
 %convolve x(t)*h(t)=y(t) Note: according to the MLS theory pager by Mark
 %Thomas, the convolution must be circular. For the intitail test, linear
 %convolution will be preformed
 
 y = conv(x,h);
-
 %add noise to the output signal
 noise = s.*(rand(length(y),1)-rand(length(y),1));
 y = y + noise;
-
 %Send y(t) through AnMLS function  y(t) -> AnMLS -> h1(t)
 
-h1 = AnalyseMLSSequence(y,0,1,N,true,0);
+h1 = AnalyseMLSSequence(y,0,reps,N,true,0);
 h1=h1(1:length(h1)-1,1);
 
 %Send y(t) though a new function that extracts h(t) from the frequency domain
 
 
-h2 = FFTImpulseExtraction(x,y);
+h2 = FFTImpulseExtraction(x,y,burst);
+
+%transpose h to match dim h1 and dim h2
+h=h';
 end
 
 %Compare h1(t), h2(t), and h(t)
 
-function h2 = FFTImpulseExtraction(x,y)
+function h2 = FFTImpulseExtraction(x,y,burst)
 %This function extracts the time domain impulse response from a given input
 %and output.
 %
@@ -86,6 +86,6 @@ H = Y./X;
 h2 = ifft(H);
 
 %Get rid of zero pads
-h2 = h2(1:length(x));
+h2 = h2(1:length(burst));
 
 end
